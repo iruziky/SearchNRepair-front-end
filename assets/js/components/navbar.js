f74278751c059c94a050902278d99c450559fe1a
iruziky <iruziky@gmail.com>
2025-05-23 18:29:36 -0300

    logo no line break

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -2,13 +2,12 @@ export function createNavbar() {
     const nav = document.createElement('div');
     nav.className = 'navbar';
     nav.innerHTML = `
-        <div class="logo">📱 SearchNRepair</div>
+        <div class="logo">📱SearchNRepair</div>
         <nav class="nav-links">
             <a href="#">Início</a>
             <a href="#">Sobre</a>
             <a href="#">Produtos</a>
             <a href="#">Contato</a>
-            <a href="#">Dashboard</a>
         </nav>
     `;
     return nav;
